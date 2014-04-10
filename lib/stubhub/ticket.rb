44b074b6a95c7feba7a751e5fd8adc61061564dd
John Nguyen <jdnguyenxo@gmail.com>
2014-04-09 23:00:11 -0400

    Update ticket.rb

--- lib/stubhub/ticket.rb
+++ lib/stubhub/ticket.rb
@@ -2,7 +2,7 @@ module Stubhub
   class Ticket < Document
 
     def self.find_by_ticket_id(ticket_id, options = {})
-      params = {"stubhubDocumentType" => "#{demodulize(self).downcase}",
+      params = {"stubhubDocumentType" => "*",
                  "id" => "#{ticket_id}"}
       Client.make_request(Ticket, params, options = {})
     end
